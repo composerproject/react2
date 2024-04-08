cc43eb27f761e95acaa7ba2635e7cae96fb0e4be
Charlotte Varlet <c.varlet@3wa.fr>
2024-04-08 11:16:54 +0200

    routes

--- router1/src/App.jsx
+++ router1/src/App.jsx
@@ -1,8 +1,14 @@
+import './App.css'
 import {Routes, Route, NavLink} from "react-router-dom"
 import Home from "./pages/Home/index.jsx";
 import Courses from "./pages/Courses/index.jsx";
 import Play from "./pages/Play/index.jsx";
-import './App.css'
+import About from "./pages/About/index.jsx";
+import Contact from "./pages/Contact/index.jsx";
+import Me from "./pages/Me/index.jsx";
+import Videos from './pages/Videos/index.jsx';
+import News from './pages/News/index.jsx';
+import AddPosts from './pages/admin/AddPosts/index.jsx';
 
 function App() {
 
@@ -12,11 +18,24 @@ function App() {
         <NavLink to='/' >Home</NavLink>
         <NavLink to='/courses' >Courses</NavLink>
         <NavLink to='/play' >Play</NavLink>
+        <NavLink to='/videos' >Videos</NavLink>
+        <NavLink to='/news' >News</NavLink>
+        <NavLink to='/about' >About us</NavLink>
+        <NavLink to='/contact' >Contact</NavLink>
+        <NavLink to='/me' >My page</NavLink>
+        <NavLink to='/admin' >Admin</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
         <Route path="/play" element={<Play />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/me" element={<Me />} />
+        
+        <Route path="/admin" element={<AddPosts />} />
       </Routes>
     </>
   )
